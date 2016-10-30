aa7cfb37dd0f95d3c865f5437c29a543ab83c1d5
mihdan <mihdan@yandex.ru>
2016-10-30 23:54:00 +0300

    Добавил ресурсы, подправил код

--- mihdan-flexslider.php
+++ mihdan-flexslider.php
@@ -22,11 +22,6 @@
  * GitHub Plugin URI: https://github.com/mihdan/mihdan-flexslider
  */
 
-function mihdan_flexslider_post_gallery( $output, $attr ) {
-	return $output;
-}
-add_filter( 'post_gallery', 'mihdan_flexslider_post_gallery', 10, 2 );
-
 /**
  * Поправим вывод тегов
  *
@@ -55,7 +50,7 @@ add_filter( 'shortcode_atts_gallery', 'mihdan_flexslider_shortcode_atts_gallery'
  */
 function mihdan_flexslider_gallery_style( $output ) {
 
-	$output = str_replace( 'gallery ', 'gallery swiper-container ', $output );
+	$output = str_replace( 'gallery ', 'gallery flexslider-container ', $output );
 
 	return $output;
 }
@@ -81,43 +76,27 @@ function mihdan_flexslider_enqueue_scripts() {
 	global $post;
 
 	if( is_a( $post, 'WP_Post' ) && has_shortcode( $post->post_content, 'gallery') ) {
-		//wp_enqueue_script( 'my-script');
-	//}
 
-	//if ( is_single() ) {
 		wp_enqueue_style( 'flexslider', plugins_url( 'assets/css/flexslider.css', __FILE__ ) );
 		wp_enqueue_style( 'mihdan-flexslider', plugins_url( 'assets/css/mihdan-flexslider-style.css', __FILE__ ) );
 		wp_enqueue_script( 'flexslider', plugins_url( 'assets/js/jquery.flexslider.js', __FILE__ ), array( 'jquery' ), null, true );
 		
 		$js = <<<JS
 			jQuery( function( $ ) {			    
-			    $('.swiper-container')
-			    	.wrapInner('<div class="swiper-wrapper"></div>')
-			    	.append('<div class="swiper-pagination"></div>')
-			    	.append('<div class="swiper-button-prev"></div>')
-			    	.append('<div class="swiper-button-next"></div>')
-			    	//.find('.gallery-item')
-			    	//.addClass('swiper-slide');
+			    $('.flexslider-container')
+			    	.wrapInner('<div class="flexslider-wrapper"></div>')
+			    	//.append('<div class="swiper-pagination"></div>')
+			    	//.append('<div class="swiper-button-prev"></div>')
+			    	//.append('<div class="swiper-button-next"></div>')			    	
 			    	
-			  var swiper = new Swiper ('.swiper-container', {
-			      loop: true,
-			      pagination: '.swiper-pagination',
-			      paginationClickable: true,
-			      grabCursor: true,
-			      nextButton: '.swiper-button-next',
-			      prevButton: '.swiper-button-prev',
-			      //effect: 'fade',
-			      //mousewheelControl: true,
-			      keyboardControl: true,
-			      hashnav: true,
-			      //autoHeight: true
-			      setWrapperSize: true,
-			      slideClass: 'gallery-item',
-			      height: 480
+			  var flexslider = $('.flexslider-container').flexslider({
+					animation: 'slide',
+					itemWidth: 638,
+					selector: '.flexslider-wrapper > .gallery-item'
 			  })  
 			});
 JS;
-		
+
 		wp_add_inline_script( 'flexslider', $js );
 	}
 }
